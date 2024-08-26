e014ca84cd8aa2f052ed47fc33080898ab7e4f61
Mohamed Belhedi <mohamed.belhedi@yadbo.com>
2024-08-26 22:27:10 +0200

    Update 1.2 change Textinput to TextArea

--- main.py
+++ main.py
@@ -4,16 +4,6 @@ from PIL import Image
 
 response = ''
 
-def ChatPrompt(api_key, user_input):
-    global response
-    genai.configure(api_key=api_key)
-
-    model = genai.GenerativeModel('gemini-1.5-flash')
-
-    # Übergabe des Bildes als PIL.Image.Image Objekt
-    response = model.generate_content([user_input])
-    return response.text
-
 def ImagePropmt(api_key, user_input, image):
     global response
     genai.configure(api_key=api_key)
@@ -24,24 +14,28 @@ def ImagePropmt(api_key, user_input, image):
     response = model.generate_content([user_input, image])
     return response.text
 
+def ChatPrompt(api_key, user_input):
+    global response
+    genai.configure(api_key=api_key)
+
+    model = genai.GenerativeModel('gemini-1.5-flash')
+
+    response = model.generate_content([user_input])
+    return response.text
+
 def main():
     st.title("Bildanalyse und Chatbot")
 
     api_key = st.text_input(label='API Key', type='password')
-    user_input = st.text_area(label='Chat')
-    pic = st.camera_input('Fotoaufnahme')
-    if st.button(label='ChatPrompt'):
-        if api_key and user_input:
-            # Sende das Bild und die Benutzereingabe an das Modell
-            chat_response = ChatPrompt(api_key, user_input)
-            st.write(chat_response)
+    user_input = st.text_area(label='Prompt Anfrage')
+    pic = st.camera_input(label='Fotoaufnahme')
     output_image = None
     if pic is not None:
         image = Image.open(pic)
         output_image = image
         st.image(image, caption='Aufgenommenes Bild', use_column_width=True)
 
-        if st.button(label='Chat'):
+        if st.button(label='Bild Analysieren'):
             if api_key and user_input:
                 # Sende das Bild und die Benutzereingabe an das Modell
                 chat_response = ImagePropmt(api_key, user_input, image)
@@ -50,8 +44,12 @@ def main():
                     st.image(output_image, caption='Aufgenommenes Bild', use_column_width=True)
             else:
                 st.warning("Es fehlt dein API Key oder deine Chat-Nachricht.")
+    if st.button(label='ChatPrompt'):
+        if api_key and user_input:
+            chat_response = ChatPrompt(api_key, user_input)
+            st.write(chat_response)
     else:
-        st.write("Frage mich etwas oder nimm ein Foto um es Analysieren")
+        st.write("Frage mich etwas oder nimm ein Foto um es zu Analysieren")
 
 if __name__ == '__main__':
     main()
